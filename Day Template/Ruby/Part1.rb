a176bd83bd1190f2be35959c77ec904ec30a8677
Joseph Peters <petersjl@rose-hulman.edu>
2023-04-27 11:49:57 -0400

    Day Template updates

--- Day Template/Ruby/Part1.rb
+++ Day Template/Ruby/Part1.rb
@@ -1,20 +1,32 @@
 require_relative "./../../Utils/RubyUtils"
+require 'test/unit/assertions'
+include Test::Unit::Assertions
 
-def main()
+def main(test = false)
     start_time = Time.now
-    solve_puzzle()
+    if test
+        run_tests()
+    else
+        puts "#{solve_puzzle()}"
+    end
+    end_time = Time.now
     end_time = Time.now
     puts "Ran in #{end_time - start_time} seconds"
 end
 
-def parse_input(test = false)
-    input = Utils.read(File.join(File.dirname(__FILE__), test ? "../testinput.txt" : "../input.txt"))
+def parse_input(str)
+    return str
+end
+
+def solve_puzzle(input = nil)
+    if not input then input = Utils.read(File.join(File.dirname(__FILE__), "../input.txt")) end
+    input = parse_input(input)
     return input
 end
 
-def solve_puzzle()
-    input = parse_input(true)
-    puts input
+def run_tests()
+    assert_equal(1, 1)
+    puts "All tests passed"
 end
 
-main()
+main(true)
